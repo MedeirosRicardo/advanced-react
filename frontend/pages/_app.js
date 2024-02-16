12e54e8a280f1b5f9c85c81b368587f7f9520d8f
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2024-02-16 01:30:15 -0500

    Styled Components and CSS

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,7 +3,7 @@ import Page from "../components/Page";
 export default function MyApp({ Component, pageProps }) {
   return (
     <Page>
-     <Component {...pageProps} /> 
+      <Component {...pageProps} />
     </Page>
   );
 }
