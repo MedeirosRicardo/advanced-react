a27e8a15ce2f603ad6b556cfca8eec210a137121
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2024-02-16 11:40:22 -0500

    Fixed Styled Components

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,11 @@
 import Page from "../components/Page";
+import nProgress from "nprogress";
+import Router from "next/router";
+import "../components/styles/nprogress.css";
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
