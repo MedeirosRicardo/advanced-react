19e9e40c0e3696e086bd825b01e45af72b917772
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2024-03-27 19:42:32 -0400

    Created Single Order Page

--- frontend/components/Checkout.js
+++ frontend/components/Checkout.js
@@ -5,6 +5,9 @@ import SickButton from "./styles/SickButton";
 import { useState } from "react";
 import nProgress from "nprogress";
 import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+import { useCart } from "../lib/cartState";
+import { CURRENT_USER_QUERY } from "./User";
 
 const CREATE_ORDER_MUTATION = gql`
   mutation CREATE_ORDER_MUTATION($token: String!) {
@@ -37,33 +40,52 @@ function CheckoutForm() {
   const [loading, setloading] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
-  const [checkout, { error: graphQLError }] = useMutation(CREATE_ORDER_MUTATION);
+  const router = useRouter();
+  const { closeCart } = useCart();
+  const [checkout, { error: graphQLError }] = useMutation(CREATE_ORDER_MUTATION,
+    {
+      refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    }
+  );
 
   async function handleSubmit(e) {
+
     // 1. Stop the form from submitting and turn the loader on
     e.preventDefault();
     setloading(true);
+
     // 2. Start the page transition
     nProgress.start();
+
     // 3. Create the payment method via stripe (If success gets the token)
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
       card: elements.getElement(CardElement),
     });
+
     // 4. Handle any errors from stripe
     if (error) {
       setError(error);
       nProgress.done();
       return;
     }
+
     // 5. Send the tolen from step 3 to our keystone server via a custom mutation
     const order = await checkout({
       variables: {
         token: paymentMethod.id
       },
     });
+
     // 6. Change the page to view the order
+    router.push({
+      pathname: '/order/[id]',
+      query: { id: order.data.checkout.id },
+    });
+
     // 7. Close the cart
+    closeCart();
+
     // 8. Turn the loader off
     setloading(false);
     nProgress.done();
